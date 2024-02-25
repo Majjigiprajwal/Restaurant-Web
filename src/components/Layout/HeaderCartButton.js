89287e114bf56809b05b9a0e43c11a82d1131839
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-02-25 23:57:25 +0530

    Making the cart functional

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,7 +3,7 @@ import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
@@ -12,6 +12,5 @@ const HeaderCartButton = (props) => {
     </button>
   );
 };
-
 
 export default HeaderCartButton;
